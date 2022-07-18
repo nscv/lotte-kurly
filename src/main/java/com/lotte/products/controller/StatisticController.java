cf6b05970c0a4f0dd6f6b92c675e3c09769ca746
chd830 <chd830@naver.com>
2022-07-18 21:37:28 +0900

    fix: 겹치는 url 제거 #21

--- src/main/java/com/lotte/products/controller/StatisticController.java
+++ src/main/java/com/lotte/products/controller/StatisticController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/statistic")
 public class StatisticController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
