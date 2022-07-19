a73ac8e8c80634fa10abebd92f568f341e8b494a
hellonayeon <hellonykwon@gmail.com>
2022-07-19 16:30:35 +0900

    style: CartController 클래스 로그 추가 #45

--- src/main/java/com/lotte/carts/controller/CartController.java
+++ src/main/java/com/lotte/carts/controller/CartController.java
@@ -2,6 +2,9 @@ package com.lotte.carts.controller;
 
 import com.lotte.carts.dto.response.CartResponseDto;
 import com.lotte.carts.service.CartService;
+import java.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CartController {
 
+    private static Logger logger = LoggerFactory.getLogger(CartController.class);
     private final CartService cartService;
 
     public CartController(CartService cartService) {
@@ -18,6 +22,7 @@ public class CartController {
 
     @GetMapping("/carts/{cartNo}")
     public ResponseEntity<CartResponseDto> getCart(@PathVariable Integer cartNo) {
+        logger.info("CartController.getCart(cartNo): {}", LocalDateTime.now());
         return ResponseEntity.ok(cartService.getCart(cartNo));
     }
 }
