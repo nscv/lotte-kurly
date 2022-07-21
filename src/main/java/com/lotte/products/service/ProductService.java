ee832e99a1570fa7b3298b524c629b96aba2b94d
choihyeongjun <chj5156@naver.com>
2022-07-21 21:38:37 +0900

    fix: 버그수정

--- src/main/java/com/lotte/products/service/ProductService.java
+++ src/main/java/com/lotte/products/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.lotte.products.service;
 
+import com.lotte.products.dto.ProductBestDto;
 import com.lotte.products.dto.ProductDto;
 import com.lotte.products.dto.ProductImgCategoryDto;
 import com.lotte.products.dto.ProductListDto;
@@ -9,10 +10,21 @@ import java.util.List;
 public interface ProductService {
     void insertProducts(ProductDto dto);
     void updateProducts(ProductDto dto);
-    List<ProductListDto> productList(String category);
-    List<ProductListDto> productHighList(String category);
-    List<ProductListDto> productLowList(String category);
+
+    List<ProductListDto> productList(ProductListDto productListDto);
+    List<ProductListDto> productHighList(ProductListDto productListDto);
+    List<ProductListDto> productLowList(ProductListDto productListDto);
     ProductImgCategoryDto productImgCategorylist(String productNo);
     List<ProductListDto> getAllProductList();
     String getProductDetail(String productNo);
+    int searchEndPage(String category);
+    List<ProductListDto>productDiscountList(ProductListDto productListDto);
+    List<ProductListDto> productAmountList(ProductListDto productListDto);
+
+    List<ProductBestDto> productBestList(ProductBestDto productBestDto);
+    int searchBestEndPage();
+    List<ProductBestDto>productBestAmountList(ProductBestDto productBestDto);
+    List<ProductBestDto>productBestHighList(ProductBestDto productBestDto);
+    List<ProductBestDto>productBestLowList(ProductBestDto productBestDto);
+    List<ProductBestDto>productBestDiscountList(ProductBestDto productBestDto);
 }
